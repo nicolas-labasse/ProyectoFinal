f668c48168ca82675bb40f553cb132826e48820c
nicolas-labasse <nicolaslabasse4@gmail.com>
2022-07-23 14:08:22 -0300

    s

--- ProyectoFinal/ProyectoFinal/settings.py
+++ ProyectoFinal/ProyectoFinal/settings.py
@@ -92,17 +92,10 @@ WSGI_APPLICATION = 'ProyectoFinal.wsgi.application'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 
-DATABASES = {
-   'default': {
-     'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'NAME': 'django_mvt',
-        'OPTIONS': { 
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+DATABASES = {   
+       'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
